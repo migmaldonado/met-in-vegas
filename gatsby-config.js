1624877c1cc70317209a90f0d9121ec5be4ee059
Miguel Maldonado <maldesan@gmail.com>
2018-10-20 23:16:23 -0400

    metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Dimension V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
+    title: "Money2020 Hackathon Project",
+    author: "Collaboration",
+    description: "A project for Money2020 Hackathon"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
